3cb8627cef17aeee7ae711b4bb987e1ca450d090
tomryb <1rybak.tomasz@gmail.com>
2023-06-01 12:49:47 +0200

    terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "MyResourceGroup"
+    storage_account_name = "tomrybstorage"
+    container_name = "tomrybcontainer"
+    key = "state.tfstate"
+  }
+}
 provider "azurerm" {
   features {}
 }
